823829fec5e903799a510cc4e9669703013f8bd5
Viktor <crazybearrrrrrr@gmail.com>
2023-01-01 23:38:05 +0200

    HW3

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const userName = prompt(`Enter your name`)
+      userAge = prompt(`Enter your age`)
+      userCity = prompt(`Enter your city`);
+
+alert(`Hello, ${userName}! You are ${userAge} years old. You are from ${userCity}`)
