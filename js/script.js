3d943e0cd94d4cc9f44c02196baa6a1168b4d1bd
Viktor <crazybearrrrrrr@gmail.com>
2023-01-03 11:01:44 +0200

    HW4

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict"
+const userName = prompt(`Enter your name`);
+const userAge = +prompt(`Enter your age`);
+const smoke = confirm(`Are you smoke?`);
+
+if (userName && userAge) {
+    if (userAge > 18 && smoke) {
+        alert(`Hi ${userName}, well, you chose this path by yourself...`)
+        console.log (`Hi ${userName}, well, you chose this path by yourself...`);
+    }     else if (userAge > 18 && smoke === false) {
+        alert(`Hi ${userName}, great! You probably also do sports`)
+        console.log (`Hi ${userName}, great! You probably also do sports`);
+    }     else if (userAge < 18 && smoke) {
+        alert(`Hi ${userName}! Do the parents know about it?`)
+        console.log (`Hi ${userName}! Do the parents know about it?`);
+    }     else if (userAge < 18 && smoke === false) {
+        alert(`Hi ${userName}, great! You probably also do sports`)
+        console.log (`Hi ${userName}, great! You probably also do sports`);
+    }
+}   else {
+        alert(`ERROR`)
+        console.log (`ERROR`);
+    }
