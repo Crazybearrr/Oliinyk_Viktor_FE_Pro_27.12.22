111b89270aa098607e016566922302f3e1d0aebb
Viktor <crazybearrrrrrr@gmail.com>
2023-01-23 21:59:57 +0200

    HW8

--- js/script.js
+++ js/script.js
@@ -1,49 +1,100 @@
-// Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+"use strict";
 
-for(let i = 20; i <= 30;){
-    console.log(i);
-    i = i + 0.5;
-}
+// Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] 
 
-// Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-const price = 27;
+// Знайти суму та кількість позитивних елементів.
 
-for(let i = 10; i <= 100; i = i + 10){
-    result = i * price;
-    console.log(result);
-}
+let positivEl = arr.filter(el => el > 0);
 
-// Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+let result = positivEl.reduce((accumulator, el) => accumulator + el, 0);
 
-let n = 9801;
+console.log(result);
+console.log(positivEl.length);
+// function sumEl(positivEl) {
+//     let sum = 0;
+//     positivEl.forEach(function(el){
+//         sum += el
+//     })
+//     return sum;
+// }
+// console.log(sumEl(positivEl));
 
-for(let i = 1; i <= 100; i++){
-   if ((Math.pow(i, 2)) <= n){
-    console.log(i);
-   }
-}
 
-// Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
+// Знайти мінімальний елемент масиву та його порядковий номер.
 
-let number = 10;
+let minNumber = Math.min(...arr);
 
-for (let i = 2; i <= number; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue;
+console.log(minNumber);
+console.log(arr.indexOf(minNumber));
+
+// Знайти максимальний елемент масиву та його порядковий номер.
+
+let maxNumber = Math.max(...arr);
+
+console.log(maxNumber);
+console.log(arr.indexOf(maxNumber));
+
+// Визначити кількість негативних елементів.
+
+let negativEl = arr.filter(el => el < 0);
+
+console.log(negativEl.length);
+
+// Знайти кількість парних позитивних елементів.
+
+let positivDuplicat = positivEl.filter((el, index, numbers) => {
+    return numbers.indexOf(el) !== index;
+});
+
+console.log(positivDuplicat.length);
+
+// Знайти кількість непарних позитивних елементів.
+
+let positivNotDuplicat = positivEl.filter(el => !positivDuplicat.includes(el));
+
+console.log(positivNotDuplicat.length);
+
+// Знайти суму парних позитивних елементів.
+
+let resultDuplicat = positivDuplicat.reduce((accumulator, el) => accumulator + el, 0);
+
+console.log(resultDuplicat);
+
+// Знайти суму непарних позитивних елементів.
+
+let resultNotDuplicat = positivNotDuplicat.reduce((accumulator, el) => accumulator + el, 0);
+
+console.log(resultNotDuplicat);
+
+// Знайти добуток позитивних елементів.
+
+// function multEl(positivEl) {
+//     let mult = 1;
+//     positivEl.forEach(function(el){
+//         mult *= el
+//     })
+//     return mult;
+// }
+// console.log(multEl(positivEl))
+
+let resultPositivEl = positivEl.reduce((accumulator, el) => accumulator * el, 1);
+
+console.log(resultPositivEl);
+
+// Знайти найбільший серед елементів масиву, остальні обнулити.
+
+let max = arr[0];
+let maxi = 0;
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+    arr[maxi] = 0;
+    maxi = i;
+  } else {
+    arr[i] = 0;
   }
-  console.log(i);
 }
 
-// Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
-
-let someNum = 12;
-
-for(let i = 2; i <= 100; i++){
-    if((3 * i) === someNum){
-        console.log(`${someNum} can be obtained by raising the number 3 to ${i} power`);
-        break;
-    } else if((3 * i) !== someNum){
-        console.log(`${someNum} cannot be obtained by raising the number 3 to the power`);
-    }   
-}
+console.log(arr);
