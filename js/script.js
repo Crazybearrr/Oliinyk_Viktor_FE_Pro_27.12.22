74f41182ab58455a57ab6c82096f6c0f93714908
Viktor <crazybearrrrrrr@gmail.com>
2023-01-27 17:19:20 +0200

    HW10.del.error

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
 
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-let key = ``;
 
 function generateKey(numb, sting){
+    let key = ``;
     for(let i = 0; i < numb; i++){
         key += sting[Math.floor(Math.random() * (sting.length))];
-    } if (key.length === 16) {
+    } if (key.length === numb) {
         console.log(key);
     }
 }
