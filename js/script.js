a5803341bfca804cbba13e20e1ac10a229bd518d
Viktor <crazybearrrrrrr@gmail.com>
2023-02-05 00:37:59 +0200

    HW11

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // Число, яке треба піднести до ступеню, передається як перший аргумент у функції;
 // Ступінь передається як другий аргумент у функціюpow (num, degree).
 
+function pow(num, degree){
+    if(degree === 1){
+        return num;
+    } else {
+        return num * pow(num, degree - 1);
+    }
+}
+
+
+console.log(pow(7, 3))
