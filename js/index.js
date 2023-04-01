58f91d20ba89334183542b6a1c802e5761248690
Viktor <crazybearrrrrrr@gmail.com>
2023-04-02 02:20:57 +0300

    HW18

--- js/index.js
+++ js/index.js
@@ -4,41 +4,57 @@
 const apiUrl = `https://jsonplaceholder.typicode.com`,
       btnSearch = document.querySelector(`#btn_search`),
       postIdInput = document.querySelector(`#postId`),
+      errorMessage = document.querySelector(`#error`),
       postWrap = document.querySelector(`#post`),
       commentWrap = document.querySelector(`#comment`);
 
-btnSearch.addEventListener(`click`, ()=>{
-  let postId = postIdInput.value;
-  // console.log(postId);
-    fetch(`${apiUrl}/posts/${postId}`)
-    .then(res => res.json())
-    .then(res => {
-      postWrap.innerHTML=`
-      <h2>${res.id}</h2>
-      <p>${res.title}</p>
-      <p>${res.body}</p>
-      <button class="btn comment" id="btn_comment" type="submit">Comment</button>`
-      const btnComment = document.querySelector(`#btn_comment`);
+btnSearch.addEventListener(`click`, (e)=>{
+  e.preventDefault()
+    let postId = postIdInput.value;
+    if(postId < 1 || postId > 100){
+        return errorMessage.classList.add(`show`);
+    }else{
+      errorMessage.classList.remove(`show`);
+
+      fetch(`${apiUrl}/posts/${postId}`)
+      .then(res => {
+        return res.ok?res.json() : new Error((`ERROR! Status: ${res.statusText}`))
+      })
+      .then(res => {
+        postWrap.innerHTML=`
+        <hr>
+        <h2>${res.title}</h2>
+        <p>${res.body}</p>
+        <button class="btn_comment" id="btn_comment" type="submit">Comment</button>`
+        const btnComment = document.querySelector(`#btn_comment`);
+          
         btnComment.addEventListener(`click`, ()=>{
           fetch(`${apiUrl}/posts/${postId}/comments`)
-          .then(res => res.json())
           .then(res => {
-            // console.log(`${apiUrl}/posts/${postId}/comments`)
+            return res.ok?res.json() : new Error((`ERROR! Status: ${res.statusText}`))
+          })
+          .then(res => {
             commentWrap.innerHTML="";
             res.forEach(el => {
               const div = document.createElement(`div`)
               div.innerHTML = `
-              <h2>${el.id}</h2>
-              <p>${el.name}</p>
+              <hr>
+              <h2>${el.name}</h2>
               <p>${el.email}</p>
               <p>${el.body}</p>`;
               commentWrap.appendChild(div)
             });
           }
-          
           )
+          .catch(error=>{
+            console.error(error)
+          })
         })
-    });
-
-    
+      })
+      .catch(error=>{
+        console.error(error),
+        postWrap.innerHTML=`
+        <p>${error.statusText}</p>`
+      })
+    }
 })
