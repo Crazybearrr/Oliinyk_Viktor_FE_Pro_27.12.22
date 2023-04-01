a4cae849fc062cd3af12534cfb8ebb5cf574667b
Viktor <crazybearrrrrrr@gmail.com>
2023-04-02 02:22:22 +0300

    HW18

--- js/index.js
+++ js/index.js
@@ -1,6 +1,3 @@
-// Використовуючи API https://jsonplaceholder.typicode.com/ зробити пошук поста за ід.
-// Ід має бути введений в інпут (валідація: ід від 1 до 100) Якщо знайдено пост, то вивести на сторінку блок з постом і зробити кнопку для отримання комкоментарів до посту.
-// Зробити завдання використовуючи проміси, перехопити помилки.
 const apiUrl = `https://jsonplaceholder.typicode.com`,
       btnSearch = document.querySelector(`#btn_search`),
       postIdInput = document.querySelector(`#postId`),
